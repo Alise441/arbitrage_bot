9fedfc530de939f54a7bf9552da17a94567958d0
Ivan <ivan.isaenko@uzh.ch>
2025-06-09 13:21:35 +0200

    adjust config to env

--- Historical analysis/config.py
+++ Historical analysis/config.py
@@ -1,11 +1,11 @@
-# config.py
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+API_KEY = os.getenv("GRAPH_API_KEY")
+if not API_KEY:
+    raise ValueError("GRAPH_API_KEY is not set in .env")
 
-# Replace with your actual API key
-API_KEY = "9dcbb5e84ceaf493ef59318aebc7ad17"
-
-SUBGRAPH_URL = (
-    f"https://gateway.thegraph.com/api/{API_KEY}/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV"
-)
+SUBGRAPH_URL = f"https://gateway.thegraph.com/api/{API_KEY}/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV"
 
 HEADERS = {
     "Authorization": f"Bearer {API_KEY}",
