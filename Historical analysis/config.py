d625ea838ef29387cf389e7b95d72e1b9126e54e
Ivan <ivan.isaenko@uzh.ch>
2025-06-09 13:08:06 +0200

    add historical analysis

---
+++ Historical analysis/config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Replace with your actual API key (no brackets)
+API_KEY = "9dcbb5e84ceaf493ef59318aebc7ad17"
+
+SUBGRAPH_URL = (
+    f"https://gateway.thegraph.com/api/{API_KEY}/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV"
+)
+
+HEADERS = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
